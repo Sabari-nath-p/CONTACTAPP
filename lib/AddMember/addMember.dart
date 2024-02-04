bdd4c30459533ed4a72d14bde851ab7af0350320
ReemaShareen <99016463+Reemasalam@users.noreply.github.com>
2024-02-04 23:50:33 +0530

    screen created

---
+++ lib/AddMember/addMember.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class addMember extends StatefulWidget {
+  const addMember({super.key});
+
+  @override
+  State<addMember> createState() => _addMemberState();
+}
+
+class _addMemberState State<addMember> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
