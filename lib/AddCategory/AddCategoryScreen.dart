32147fb7eeab675f76d852186fb23b5087d82d17
Akleema m javed <akleemamj@gmail.com>
2024-02-06 23:03:29 +0530

    change

--- lib/AddCategory/AddCategoryScreen.dart
+++ lib/AddCategory/AddCategoryScreen.dart
@@ -3,23 +3,19 @@ import 'package:flutter/material.dart';
 class AddCategoryScreen extends StatelessWidget {
   const AddCategoryScreen({super.key});
 
-  @over
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Column(
-        children: [
-          AppBar (title:Text("Add Category"), 
-          backgroundColor:Color.fromRGBO(191, 120, 43, 1),
-          centerTitle: true,
-          
-          
-          
-          
-            
-          
-              ),
-        ],
-      )
+      appBar: AppBar (title:Text("Add Category"), 
+      backgroundColor:Color.fromRGBO(191, 120, 43, 1),
+      centerTitle: true,
+      
+      
+      
+
+        
+      
+    )
     );
   }
 }
