81fb75c388dee009c98ac14a561e14537f02293c
unknown <nandanasanal321@gmail.com>
2024-02-07 20:22:20 +0530

    add

--- lib/Tags/tags.dart
+++ lib/Tags/tags.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-class TagScreen extends StatefulWidget {
-  const TagScreen({super.key});
-
-  @override
-  State<TagScreen> createState() => _TagScreenState();
-}
-
-class _TagScreenState extends State<TagScreen> {
-  @override
-=======
 class TagScreen extends StatelessWidget {
   const TagScreen({super.key});
 
   @override
->>>>>>> 4407ebf13695e5a34c2afbe9c6bc7d10e32577db
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
