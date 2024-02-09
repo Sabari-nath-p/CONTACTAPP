2a3dcad90cdcf4cb67d09cd09b074c21bec26ead
Neynu u <nayanaraj578@gmail.com>
2024-02-09 10:53:01 +0530

    new project

--- lib/AddMember/addMember.dart
+++ lib/AddMember/addMember.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class AddMemberScreen extends StatelessWidget {
   const AddMemberScreen({super.key});
@@ -6,11 +7,30 @@ class AddMemberScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Add Member'),backgroundColor: Color.fromRGBO(191, 120, 43, 1),
-
-),
-      
-      
+      appBar: AppBar(
+        title: Text('Add Member'),
+        backgroundColor: Color.fromRGBO(191, 120, 43, 1),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 6.470.h,
+          ),
+          Container(
+            height: 8.49.h,
+            width: 95.94.w,
+            margin: EdgeInsets.only(left: 2.w),
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 5.368.w),
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(253, 244, 235, 1),
+              borderRadius: BorderRadius.circular(1.1.w),
+            ),
+            child: Text("Name" , ),
+          ),
+        ],
+      ),
     );
   }
 }
