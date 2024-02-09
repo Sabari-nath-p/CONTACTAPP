1f77dbdfa2b68658cee1dcf33367af1c1ea80854
muhammed-fayaz <99132360+muhammed-fayaz@users.noreply.github.com>
2024-02-09 22:05:26 +0530

    done

--- lib/EventScreen.dart
+++ lib/EventScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sizer/sizer.dart';
 
 class EventScreen extends StatefulWidget {
-  const EventScreen({super.key});
+  const EventScreen({Key? key}) : super(key: key);
 
   @override
   State<EventScreen> createState() => _EventScreenState();
@@ -11,10 +13,64 @@ class _EventScreenState extends State<EventScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          
-        ],
+      appBar: AppBar(
+        backgroundColor: Color(0xffBF782B),
+        centerTitle: true,
+        title: Row(
+          children: [
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(top: 3.h),
+                alignment: Alignment.center,
+                child: Text(
+                  'Events',
+                  style: GoogleFonts.karma(
+                    color: Colors.white,
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.add),
+              iconSize: 35,
+              color: Colors.white,
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ),
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsetsDirectional.only(top: 50),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Select The Event To Add Contact",
+                  style: GoogleFonts.karma(
+                    fontSize: 13.sp,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0XFFA56219),
+                  ),
+                ),
+                SizedBox(height: 20), 
+                Text(
+                  "Event Held Year:",
+                  style: GoogleFonts.karma(
+                    fontSize: 13.sp,
+                    fontWeight: FontWeight.w600,
+                    color: Color(0XFFA56219),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
