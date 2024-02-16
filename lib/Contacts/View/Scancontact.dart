812a85fdaaba3f92f665b84e62ffc857e123858d
ReemaShareen <99016463+Reemasalam@users.noreply.github.com>
2024-02-16 17:44:31 +0530

    fix

---
+++ lib/Contacts/View/Scancontact.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Scancontact extends StatelessWidget {
+  const Scancontact({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp();
+  }
+}
