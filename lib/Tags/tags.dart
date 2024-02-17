4d410508e94b325e9e11790bd1d8c0b4c4cb8fcc
Sabarinath P <sabarinath5604@gmail.com>
2024-02-17 21:39:21 +0530

    new code

--- lib/Tags/tags.dart
+++ lib/Tags/tags.dart
@@ -28,55 +28,6 @@ class TagScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [_ItemCard("High"), _ItemCard("Medium")],
         ),
-<<<<<<< HEAD
-=======
-        centerTitle: true,
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 9.1.h,
-            width: 90.2.w,
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 4.3.w),
-            color: Color.fromRGBO(253, 244, 235, 1),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'High',
-                  style: TextStyle(
-                      fontSize: 20.0, color: Color.fromRGBO(165, 98, 25, 1)),
-                ),
-                Container(
-                  width: 6.5.h,
-                  height: 13.6.h,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.red),
-                  child: Icon(Icons.delete),
-                )
-              ],
-            ),
-          ),
-          SizedBox(
-            height: 2.0.h,
-          ),
-          Container(
-            height: 9.1.h,
-            width: 201.3.w,
-            color: Color.fromRGBO(253, 244, 235, 1),
-            child: const Text(
-              'medium',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Color.fromRGBO(165, 98, 25, 1),
-              ),
-            ),
-          )
-        ],
->>>>>>> 713cc35cc1ec6bfb6f3466bf8a474adeb6d122db
       ),
     );
   }
